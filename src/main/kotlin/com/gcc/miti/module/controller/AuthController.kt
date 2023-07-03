f2a51bb140dc69faf7d96591e7c46fc492e825e9
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-07-03 17:07:46 +0900

    feat: 로그인 리프래쉬 구현

--- src/main/kotlin/com/gcc/miti/module/controller/AuthController.kt
+++ src/main/kotlin/com/gcc/miti/module/controller/AuthController.kt
@@ -1,5 +1,7 @@
 package com.gcc.miti.module.controller
 
+import com.gcc.miti.module.dto.authDto.SignInDto
+import com.gcc.miti.module.dto.authDto.TokenDto
 import com.gcc.miti.module.entity.Verification
 import com.gcc.miti.module.service.AuthService
 import org.springframework.web.bind.annotation.*
@@ -13,7 +15,20 @@ class AuthController(private val authService: AuthService) {
     }
 
     @GetMapping("{email}")
-    fun checkCertification(@PathVariable(name = "email") email: String, @RequestParam certificationNumber: String):Boolean {
+    fun checkCertification(
+        @PathVariable(name = "email") email: String,
+        @RequestParam certificationNumber: String,
+    ): Boolean {
         return authService.checkCertification(email, certificationNumber)
+    }
+
+    @PostMapping("/sign-in")
+    fun signIn(@RequestBody signInDto: SignInDto): TokenDto {
+        return authService.signIn(signInDto)
+    }
+
+    @PostMapping("/refresh")
+    fun refresh(@RequestBody tokenDto: TokenDto): TokenDto {
+        return authService.refresh(tokenDto)
     }
 }
--- src/main/kotlin/com/gcc/miti/module/service/AuthService.kt
+++ src/main/kotlin/com/gcc/miti/module/service/AuthService.kt
@@ -1,16 +1,27 @@
 package com.gcc.miti.module.service
 
+import com.gcc.miti.module.dto.authDto.SignInDto
+import com.gcc.miti.module.dto.authDto.TokenDto
+import com.gcc.miti.module.entity.RefreshToken
 import com.gcc.miti.module.entity.Verification
+import com.gcc.miti.module.global.exception.BaseException
+import com.gcc.miti.module.global.exception.BaseExceptionCode
+import com.gcc.miti.module.global.security.JwtTokenProvider
+import com.gcc.miti.module.repository.RefreshTokenRepository
 import com.gcc.miti.module.repository.VerificationRepository
-import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AuthService(
     private val verificationRepository: VerificationRepository,
-    private val javaMailSender: JavaMailSender,
     private val mailService: MailService,
+    private val tokenProvider: JwtTokenProvider,
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val authenticationManagerBuilder: AuthenticationManagerBuilder,
 ) {
     @Transactional
     fun saveMail(email: String): Verification {
@@ -27,6 +38,30 @@ class AuthService(
             return true
         } else {
             return false
+        }
+    }
+
+    fun signIn(signInDto: SignInDto): TokenDto {
+        signInDto.apply {
+            val credential = UsernamePasswordAuthenticationToken(userId, password)
+            val authentication = authenticationManagerBuilder.`object`.authenticate(credential)
+            val token = tokenProvider.createToken(authentication)
+            refreshTokenRepository.save(RefreshToken(userId, token.refreshToken))
+            return token
+        }
+    }
+
+    fun refresh(tokenDto: TokenDto): TokenDto {
+        val userId = tokenProvider.getUserPk(tokenDto.accessToken)
+        val savedRefreshToken = refreshTokenRepository.findByIdOrNull(userId)
+        if (tokenDto.refreshToken == savedRefreshToken?.refreshToken) {
+            val authentication = tokenProvider.getAuthentication(tokenDto.accessToken)
+            val token = tokenProvider.createToken(authentication)
+            return TokenDto(token.accessToken, token.refreshToken).also {
+                refreshTokenRepository.save(RefreshToken(userId, token.refreshToken))
+            }
+        } else {
+            throw BaseException(BaseExceptionCode.REFRESH_TOKEN_MISMATCH)
         }
     }
 }

c42d551ef9b94d22e0334c3fcb567e8f073c1d05
jickDo <pocard0117@naver.com>
2023-07-06 15:42:43 +0900

    checkout 전 저장용

--- src/main/kotlin/com/gcc/miti/module/entity/Group.kt
+++ src/main/kotlin/com/gcc/miti/module/entity/Group.kt
@@ -1,13 +1,20 @@
 package com.gcc.miti.module.entity
 
 import java.time.LocalDateTime
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.Table
 
 @Entity
 @Table(name = "my_group")
 class Group(
     val description: String,
-
+    val title: String,
     val maxUsers: Short,
 
 ) : BaseTimeEntity() {
@@ -16,6 +23,8 @@ class Group(
     val id: Long = 0
 
     val meetDate: LocalDateTime? = null
+
+    val meetPlace: String? = null
 
     @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
     val groupMembers: List<User> = listOf()
--- src/main/kotlin/com/gcc/miti/module/repository/VerificationRepository.kt
+++ src/main/kotlin/com/gcc/miti/module/repository/VerificationRepository.kt
@@ -2,6 +2,7 @@ package com.gcc.miti.module.repository
 
 import com.gcc.miti.module.entity.ChatMessage
 import com.gcc.miti.module.entity.Verification
+import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 

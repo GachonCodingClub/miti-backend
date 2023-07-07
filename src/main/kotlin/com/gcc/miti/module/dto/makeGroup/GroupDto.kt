8af2d6a3b29a5bb3308a4892f38ed56ff2941ff2
jickDo <pocard0117@naver.com>
2023-07-07 18:07:05 +0900

    1. makeGroup 서비스와 컨트롤러 생성 2. 참여자 엔티티 만들고 연관후 주석처리 3. 그룹과 유저 연관설정성공

---
+++ src/main/kotlin/com/gcc/miti/module/dto/makeGroup/GroupDto.kt
@@ -0,0 +1,27 @@
+package com.gcc.miti.module.dto.makeGroup
+
+import com.gcc.miti.module.entity.Group
+import com.gcc.miti.module.entity.User
+import java.time.LocalDateTime
+
+data class GroupDto(
+    val description: String,
+    val title: String,
+    val maxUsers: Short,
+    val meetDate: LocalDateTime?,
+    val meetPlace: String?,
+) {
+    companion object {
+        fun toGroup(groupDto: GroupDto, user: User): Group {
+            return Group(
+                groupDto.description,
+                groupDto.title,
+                groupDto.maxUsers,
+            ).also {
+                it.meetDate = groupDto.meetDate
+                it.meetPlace = groupDto.meetPlace
+                it.leader = user // 이부분 다시볼것
+            }
+        }
+    }
+}

d649561e0448522d9f862161c78fd68195121c24
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-09-06 09:20:50 +0900

    feat: auto deployment

--- src/test/kotlin/com/gcc/miti/MitiApplicationTests.kt
+++ src/test/kotlin/com/gcc/miti/MitiApplicationTests.kt
@@ -1,12 +1,6 @@
 package com.gcc.miti
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MitiApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-}
+class MitiApplicationTests

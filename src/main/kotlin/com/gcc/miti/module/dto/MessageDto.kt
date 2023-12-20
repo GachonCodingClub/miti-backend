392fa9ef6807eb1de3215e14a4cd072abbc3564f
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-12-20 22:00:14 +0900

    refactor: refactor chat

--- src/main/kotlin/com/gcc/miti/module/dto/MessageDto.kt
+++ src/main/kotlin/com/gcc/miti/module/dto/MessageDto.kt
@@ -1,13 +1,16 @@
 package com.gcc.miti.module.dto
 
+import com.gcc.miti.module.entity.User
+
 data class MessageDto(
     val message: String,
     val sender: String,
     val groupId: String,
 ) {
-    fun toDto(): ChatMessageDto {
+    fun toDto(user: User): ChatMessageDto {
         return ChatMessageDto(
-            sender,
+            user.userId,
+            user.nickname,
             message,
         )
     }

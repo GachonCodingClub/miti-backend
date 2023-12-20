e01d721cbe808df47dc1ede8931e2339dd331f64
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-12-20 15:08:30 +0900

    feat: add my profile

--- src/main/kotlin/com/gcc/miti/module/constants/Weight.kt
+++ src/main/kotlin/com/gcc/miti/module/constants/Weight.kt
@@ -1,6 +1,18 @@
 package com.gcc.miti.module.constants
 
-enum class Weight {
-    A,
-    B,
+enum class Weight(val min: Int, val max: Int) {
+    A(40, 50),
+    B(50, 60),
+    C(60, 70),
+    D(70, 80),
+    E(80, 90),
+    F(90, 100),
+    HIDDEN(0,0);
+    fun toMinMaxString(): String {
+        return if(this.name != "HIDDEN"){
+            "$min~$max"
+        }else{
+            "비공개"
+        }
+    }
 }
---
+++ src/main/kotlin/com/gcc/miti/module/global/security/Principal.kt
@@ -0,0 +1,5 @@
+package com.gcc.miti.module.global.security
+
+class Principal(
+    val id: String
+)

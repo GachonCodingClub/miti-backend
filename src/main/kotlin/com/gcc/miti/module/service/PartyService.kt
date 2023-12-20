8b5c2ec3d8ac9e59163721c6a4761c2d0afd0aa0
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2023-12-20 11:09:04 +0900

    feat: add party by nickname when create group

--- src/main/kotlin/com/gcc/miti/module/service/PartyService.kt
+++ src/main/kotlin/com/gcc/miti/module/service/PartyService.kt
@@ -1,40 +1,24 @@
 package com.gcc.miti.module.service
 
 import com.gcc.miti.module.dto.partydto.PartyDto
-import com.gcc.miti.module.dto.partydto.PartyListDto
 import com.gcc.miti.module.entity.Party
 import com.gcc.miti.module.global.exception.BaseException
 import com.gcc.miti.module.global.exception.BaseExceptionCode
 import com.gcc.miti.module.repository.GroupRepository
-import com.gcc.miti.module.repository.PartyListRepository
+import com.gcc.miti.module.repository.PartyMemberRepository
 import com.gcc.miti.module.repository.PartyRepository
 import com.gcc.miti.module.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import javax.persistence.EntityNotFoundException
 
 @Service
 class PartyService(
-    private val partyListRepository: PartyListRepository,
+    private val partyMemberRepository: PartyMemberRepository,
     private val userRepository: UserRepository,
     private val partyRepository: PartyRepository,
     private val groupRepository: GroupRepository,
 
-) {
-    @Transactional
-    fun makePartyList(userId: String, partyId: Long): Boolean {
-        val party = partyRepository.findById(partyId)
-            .orElseThrow { throw EntityNotFoundException("Party not found") }
-        val user = userRepository.getReferenceById(userId)
-        partyListRepository.save(
-            PartyListDto.toPartyList(
-                user,
-                party,
-            ),
-        )
-        return true
-    }
-
+    ) {
     @Transactional
     fun makeParty(partyDto: PartyDto, userId: String, groupId: Long): Boolean {
         val group = groupRepository.getReferenceById(groupId)

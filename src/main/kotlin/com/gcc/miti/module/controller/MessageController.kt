c5cf15c83dcf6f79a0aa4a1766d777aebc3875c7
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-01-20 23:25:26 +0900

    feat: chat paging

--- src/main/kotlin/com/gcc/miti/module/controller/MessageController.kt
+++ src/main/kotlin/com/gcc/miti/module/controller/MessageController.kt
@@ -2,6 +2,8 @@ package com.gcc.miti.module.controller
 
 import com.gcc.miti.module.dto.ChatMessageDto
 import com.gcc.miti.module.repository.ChatMessageRepository
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,6 +20,15 @@ class MessageController(
     @Transactional(readOnly = true)
     fun getAllMessages(@PathVariable(name = "groupId") groupId: Long): List<ChatMessageDto> {
         return chatMessageRepository.findAllByGroup_IdOrderByCreatedAt(groupId).map {
+            ChatMessageDto.chatMessageToDto(it)
+        }
+    }
+
+    @GetMapping("/{groupId}/page")
+    fun getAllMessagesPageable(@PathVariable(name = "groupId") groupId: Long,
+    @PageableDefault(page = 0, size = 10) pageable: Pageable
+    ): List<ChatMessageDto> {
+        return chatMessageRepository.findAllByGroup_IdOOrderByCreatedAtDesc(groupId, pageable).map {
             ChatMessageDto.chatMessageToDto(it)
         }
     }

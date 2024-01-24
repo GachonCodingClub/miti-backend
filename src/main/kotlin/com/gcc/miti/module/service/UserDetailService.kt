058191282eb33fc3d784d442dc8731549e9ab485
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-01-24 20:40:18 +0900

    feat: return timestamp on chatting

--- src/main/kotlin/com/gcc/miti/module/service/UserDetailService.kt
+++ src/main/kotlin/com/gcc/miti/module/service/UserDetailService.kt
@@ -1,20 +1,20 @@
 package com.gcc.miti.module.service
 
+import com.gcc.miti.module.global.exception.BaseException
+import com.gcc.miti.module.global.exception.BaseExceptionCode
 import com.gcc.miti.module.repository.UserRepository
-import org.springframework.http.HttpStatus
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpServerErrorException
 import java.util.Collections
 
 @Service
 class UserDetailService(private val userRepository: UserRepository) : UserDetailsService {
     override fun loadUserByUsername(username: String): UserDetails {
-        val user = userRepository.findById(username).orElseThrow { HttpServerErrorException(HttpStatus.NOT_FOUND) }
+        val user = userRepository.findById(username).orElseThrow { BaseException(BaseExceptionCode.NOT_FOUND) }
         val grantedAuthority: GrantedAuthority = SimpleGrantedAuthority("USER")
         return User(user.userId, user.password, Collections.singleton(grantedAuthority))
     }

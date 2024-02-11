e4a7689a972e49e332f63f59846274c39c564866
jeonghyeon00 <sjh00kr@naver.com>
2024-02-11 22:37:03 +0900

    feat: 회원탈퇴 추가

--- src/main/kotlin/com/gcc/miti/module/service/UserService.kt
+++ src/main/kotlin/com/gcc/miti/module/service/UserService.kt
@@ -13,19 +13,25 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class UserService(private val userRepository: UserRepository) {
     @Transactional(readOnly = true)
-    fun getMyProfile():ProfileRes{
+    fun getMyProfile(): ProfileRes {
         val userId = SecurityUtils.getUserIdFromJwt()
         val user = userRepository.findByIdOrNull(userId) ?: throw BaseException(BaseExceptionCode.USER_NOT_FOUND)
         return ProfileRes.userToProfileRes(user)
     }
 
     @Transactional
-    fun updateProfile(updateProfileReq: UpdateProfileReq){
+    fun updateProfile(updateProfileReq: UpdateProfileReq) {
         val userId = SecurityUtils.getUserIdFromJwt()
         val user = userRepository.getReferenceById(userId)
         user.description = updateProfileReq.description
         user.nickname = updateProfileReq.nickname
         user.height = updateProfileReq.height
         user.weight = updateProfileReq.weight
+    }
+
+    @Transactional
+    fun deleteUser(userId: String) {
+        val user = userRepository.getReferenceById(userId)
+        userRepository.delete(user)
     }
 }

8c6ca2381a9bd6b2b66526178a4620793bab9464
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-19 21:55:45 +0900

    fix: fix height, weight

--- src/main/kotlin/com/gcc/miti/module/dto/UserSummaryDto.kt
+++ src/main/kotlin/com/gcc/miti/module/dto/UserSummaryDto.kt
@@ -10,8 +10,8 @@ class UserSummaryDto(
     val userId: String,
     val userName: String,
     val gender: Gender,
-    val height: Height,
-    val weight: Weight,
+    val height: String,
+    val weight: String,
     val age: Int,
     val description: String?,
 ) {
@@ -22,8 +22,8 @@ class UserSummaryDto(
                     userId,
                     userName,
                     gender,
-                    height,
-                    weight,
+                    height.toMinMaxString(),
+                    weight.toMinMaxString(),
                     LocalDate.now().year - birthDate.year,
                     description,
                 )

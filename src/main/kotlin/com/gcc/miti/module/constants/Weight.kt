60d257480b215908cf2611a11c6215d3e46ec9fc
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-19 22:24:39 +0900

    fix: height and weight

--- src/main/kotlin/com/gcc/miti/module/constants/Weight.kt
+++ src/main/kotlin/com/gcc/miti/module/constants/Weight.kt
@@ -1,12 +1,16 @@
 package com.gcc.miti.module.constants
 
 enum class Weight(val min: Int, val max: Int) {
-    A(40, 50),
-    B(50, 60),
-    C(60, 70),
-    D(70, 80),
-    E(80, 90),
-    F(90, 100),
+    A(30, 39),
+    B(40, 49),
+    C(50, 59),
+    D(60, 69),
+    E(70, 79),
+    F(80, 89),
+    G(90, 99),
+    H(100, 109),
+    I(110, 119),
+    J(120, 129),
     HIDDEN(0,0);
     fun toMinMaxString(): String {
         return if(this.name != "HIDDEN"){

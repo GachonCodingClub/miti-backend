db4783c81b6032846a8ee52c325b328f489afaf7
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-24 13:50:23 +0900

    fix: fix report

--- src/main/kotlin/com/gcc/miti/report/controller/ReportController.kt
+++ src/main/kotlin/com/gcc/miti/report/controller/ReportController.kt
@@ -1,8 +1,10 @@
 package com.gcc.miti.report.controller
 
+import com.gcc.miti.auth.security.GetIdFromToken
 import com.gcc.miti.report.dto.ReportReq
 import com.gcc.miti.report.service.ReportService
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +16,8 @@ class ReportController(private val reportService: ReportService) {
 
     @Operation(summary = "신고")
     @PostMapping("")
-    fun report(@RequestBody request: ReportReq){
-        return reportService.report(request)
+    fun report(@RequestBody request: ReportReq,
+               @Parameter(hidden = true) @GetIdFromToken userId: String){
+        return reportService.report(request, userId)
     }
 }

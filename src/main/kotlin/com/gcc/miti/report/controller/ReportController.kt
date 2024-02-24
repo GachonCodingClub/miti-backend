32946af8f2f6373d8ee72ba265c077b146f995d2
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-24 12:34:41 +0900

    docs: add swagger summary

--- src/main/kotlin/com/gcc/miti/report/controller/ReportController.kt
+++ src/main/kotlin/com/gcc/miti/report/controller/ReportController.kt
@@ -2,6 +2,7 @@ package com.gcc.miti.report.controller
 
 import com.gcc.miti.report.dto.ReportReq
 import com.gcc.miti.report.service.ReportService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/report")
 class ReportController(private val reportService: ReportService) {
 
+    @Operation(summary = "신고")
     @PostMapping("")
     fun report(@RequestBody request: ReportReq){
         return reportService.report(request)

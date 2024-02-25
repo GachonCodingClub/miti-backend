b22e915953fdb201f598d19d1d3e780d959e995b
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-25 16:26:55 +0900

    feat: change putToken Request type

--- src/main/kotlin/com/gcc/miti/notification/controller/NotificationController.kt
+++ src/main/kotlin/com/gcc/miti/notification/controller/NotificationController.kt
@@ -1,16 +1,17 @@
 package com.gcc.miti.notification.controller
 
+import com.gcc.miti.notification.dto.NotificationTokenRequest
 import com.gcc.miti.notification.service.NotificationService
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/notification")
 class NotificationController(private val notificationService: NotificationService) {
     @PutMapping("/token")
-    fun putToken(@RequestParam("token", required = true) token: String) {
-        return notificationService.putToken(token)
+    fun putToken(@RequestBody request: NotificationTokenRequest) {
+        return notificationService.putToken(request)
     }
 }

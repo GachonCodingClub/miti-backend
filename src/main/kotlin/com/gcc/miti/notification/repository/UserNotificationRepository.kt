9d071e7afa53c9de678a4c6c5c56927168f4d400
SongJeongHyeon <56068392+jeonghyeon00@users.noreply.github.com>
2024-02-26 23:46:15 +0900

    feat: delete same token in another account

--- src/main/kotlin/com/gcc/miti/notification/repository/UserNotificationRepository.kt
+++ src/main/kotlin/com/gcc/miti/notification/repository/UserNotificationRepository.kt
@@ -2,8 +2,11 @@ package com.gcc.miti.notification.repository
 
 import com.gcc.miti.notification.entity.UserNotification
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserNotificationRepository: JpaRepository<UserNotification, String> {
+    @Modifying
+    fun deleteByToken(token: String): Int
 }

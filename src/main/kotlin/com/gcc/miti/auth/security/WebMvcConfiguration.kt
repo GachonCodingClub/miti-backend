ab74830f2b92f3d86415a7b25cdb54db5e5a6427
jeonghyeon00 <sjh00kr@naver.com>
2024-02-28 18:52:59 +0900

    fix: fix cors

---
+++ src/main/kotlin/com/gcc/miti/auth/security/WebMvcConfiguration.kt
@@ -0,0 +1,14 @@
+package com.gcc.miti.auth.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfiguration(): WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("*")
+    }
+}
